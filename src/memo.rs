dcc4822c58e41962da612a5691d1a66f2681a5b7
mithrandir42 <code@mithrandir42.de>
2023-07-11 05:48:33 +0200

    changed inner structure of Memo

--- src/memo.rs
+++ src/memo.rs
@@ -3,7 +3,6 @@ use serde_derive::{Serialize};
 
 use multimap::MultiMap;
 
-pub type Key = String;
 pub type Value = String;
 
 
@@ -15,50 +14,49 @@ pub type Value = String;
 
 
 #[derive(Debug, Clone, PartialEq, Serialize)]
-pub struct Memo {
-    collection: Key,            // a schema
-    title: Value,               // an id
-    data: MultiMap<Key, Value>, // the actual information
+pub struct Memo<'a> {
+    data: MultiMap<&'a str, Value>, // the actual information
 }
 
-impl Memo {
+impl<'a> Memo<'a> {
     pub fn new<K, V>(collection: K, title: V) -> Self
     where
-        K: Into<Key>,
+        K: Into<&'a str>,
         V: Into<Value>,
     {
+        let mut data: MultiMap<&'a str, Value> = Default::default();
+        data.insert("mr:id", title.into());
+        data.insert("mr:schema", Value::from(collection.into()));
         Self {
-            collection: collection.into(),
-            title: title.into(),
-            data: Default::default(),
+            data,
         }
     }
 
-    pub fn title(&self) -> String {
-        self.title.to_string()
+    pub fn title(&'a self) -> &'a str {
+        self.data.get("mr:id").unwrap()
     }
 
-    pub fn collection(&self) -> String {
-        self.collection.clone()
+    pub fn collection(&'a self) -> &'a str {
+        self.data.get("mr:schema").unwrap()
     }
 
-    pub fn data(&self) -> &MultiMap<Key, Value> {
+    pub fn data(&self) -> &MultiMap<&'a str, Value> {
         &self.data
     }
 
     pub fn insert<K, V>(&mut self, key: K, value: V)
     where
-        K: Into<Key>,
+        K: Into<&'a str>,
         V: Into<Value>,
     {
         let key = key.into();
         let value = value.into();
-        self.data.insert(key.clone(), value.clone()); // TODO: TBR
+        self.data.insert(key.into(), value.clone()); // TODO: TBR
     }
 
     pub fn insert_many<K, V, I>(&mut self, key: K, values: I)
     where
-        K: Into<Key>,
+        K: Into<&'a str>,
         V: Into<Value>,
         I: IntoIterator<Item = V>,
     {
@@ -66,13 +64,13 @@ impl Memo {
         let values = values.into_iter();
         
         self.data  // TODO: TBR
-            .insert_many(key.clone(), values.map(V::into));
+            .insert_many(key.into(), values.map(V::into));
 
     }
 
     pub fn with<K, V>(mut self, key: K, value: V) -> Self
     where
-        K: Into<Key>,
+        K: Into<&'a str>,
         V: Into<Value>,
     {
         self.insert(key, value);
@@ -81,7 +79,7 @@ impl Memo {
 
     pub fn with_many<K, V, I>(mut self, key: K, values: I) -> Self
     where
-        K: Into<Key>,
+        K: Into<&'a str>,
         V: Into<Value>,
         I: IntoIterator<Item = V>,
     {
@@ -90,9 +88,9 @@ impl Memo {
     }
 }
 
-impl Display for Memo {
+impl<'a> Display for Memo<'a> {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
-        writeln!(f, "@{} {}", self.collection, self.title)?;
+        writeln!(f, "@{} {}", self.collection(), self.title())?;
         for (key, values) in self.data.iter_all() {
             for value in values {
                 writeln!(f, ".{} {}", key, value)?;
@@ -114,8 +112,8 @@ mod test_memo {
             .with("character", "Samwise Gamgee")
             .with("character", "Gandalf the Gray");
 
-        assert_eq!(memo.collection, "book".to_string());
-        assert_eq!(memo.title, "The Lord of the Rings".to_string());
+        assert_eq!(memo.collection(), "book");
+        assert_eq!(memo.title(), "The Lord of the Rings");
         assert_eq!(memo.data.len(), 2);
         //assert_eq!(memo.data.total_len(), 4); // DOES NOT EXIST IN MULTIMAP
         assert_eq!(
